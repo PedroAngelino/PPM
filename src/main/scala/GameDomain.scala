d8746aef485bda6ab7abd71218ca3bc9fe2b3fde
PedroAngelino <103763819+PedroAngelino@users.noreply.github.com>
2026-05-17 22:51:41 +0100

    Muidad asd jeseawda

---
+++ src/main/scala/GameDomain.scala
@@ -0,0 +1,21 @@
+import scala.collection.parallel.immutable.ParMap
+
+object GameDomain {
+
+  type Coord2D = (Int, Int) // linha, coluna
+  type Board = ParMap[Coord2D, Stone]
+
+  enum Stone:
+    case Black, White
+
+    def opponent: Stone = this match
+      case Black => White
+      case White => Black
+
+  def hasStone(board: Board)(coord: Coord2D): Boolean = board.contains(coord)
+
+  def getStone(board: Board)(coord: Coord2D): Option[Stone] = board.get(coord)
+
+  def isOwned(board: Board)(player: Stone)(coord: Coord2D): Boolean =
+    board.get(coord).contains(player)
+}
--- src/main/scala/T6.scala
+++
@@ -1,5 +0,0 @@
-object T6 {
-  def isTimeOver(startTime: Long,limitMillis: Int): Boolean = {
-    (System.currentTimeMillis() - startTime) > limitMillis
-  }
-}
